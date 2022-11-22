c7e78c6ea211730640e3f092155c252f9b47c40d
Lloyd Grogan <lloydgrogan@affinidi.com>
2022-11-22 14:30:12 +0100

    feat(FTL-8002):updating iteration

--- src/features/issuance/json-schema/downloadSchemaJson.ts
+++ src/features/issuance/json-schema/downloadSchemaJson.ts
@@ -3,7 +3,6 @@ import { window, workspace } from 'vscode'
 import fetch from 'node-fetch'
 import { iamHelpers } from '../../iam/iamHelpers'
 import { schemaManagerHelper } from '../../schema-manager/schemaManagerHelper'
-import { issuanceHelper } from '../IssuanceHelper'
 
 const getSummary = async () => {
   const projectId = await iamHelpers.askForProjectId()
@@ -23,24 +22,6 @@ const getSummary = async () => {
   }
 }
 
-export const getSearchedOffers = async (input: { issuanceId: string }, options: Options) => {
-  const summary = await getSummary()
-  if (!summary) {
-    return undefined
-  }
-
-  const issuanceId = await issuanceHelper.askForIssuanceId(
-    { projectId: summary?.projectId },
-    { apiKeyHash: summary?.apiKeyHash },
-  )
-  if (!issuanceId) {
-    return undefined
-  }
-
-  const searchOffers = await seachOffers(input, options)
-  return searchOffers
-}
-
 export const downloadSchemaFile = async (options: Options) => {
   const summary = await getSummary()
   if (!summary) {
@@ -49,10 +30,9 @@ export const downloadSchemaFile = async (options: Options) => {
 
   const schema = await schemaManagerHelper.askForMySchema({ did: summary?.did }, options)
 
-  const schemaObject = await fetch(`${schema?.jsonSchemaUrl}`)
+  const downloadableSchemaObject = await fetch(`${schema?.jsonSchemaUrl}`)
     .then((res) => res.json())
     .then((out) => {
-      console.log('FullSchema: ', JSON.stringify(out, null, '\t'))
       const { credentialSubject } = out.properties
       return credentialSubject
     })
@@ -61,7 +41,7 @@ export const downloadSchemaFile = async (options: Options) => {
   return window.showTextDocument(
     await workspace.openTextDocument({
       language: 'plaintext',
-      content: JSON.stringify(schemaObject, null, '\t'),
+      content: JSON.stringify(downloadableSchemaObject, null, '\t'),
     }),
   )
 }
