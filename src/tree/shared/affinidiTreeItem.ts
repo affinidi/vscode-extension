5bb849dcb0a61caac71db750603260498821ac72
Vitaly Rudenko <vitaly.r@affinidi.com>
2022-12-01 16:32:44 +0200

    fix: added more features to AffinidiTreeItem

--- src/tree/shared/affinidiTreeItem.ts
+++ src/tree/shared/affinidiTreeItem.ts
@@ -2,9 +2,11 @@ import { TreeItem, TreeItemCollapsibleState, ThemeIcon, Command } from 'vscode'
 
 export type AffinidiTreeItemInput = {
   label: string
+  description?: string
   state?: TreeItemCollapsibleState
   icon?: ThemeIcon
   command?: string | Command
+  contextValue?: string
 }
 
 export class AffinidiTreeItem extends TreeItem {
@@ -12,6 +14,9 @@ export class AffinidiTreeItem extends TreeItem {
     super(input.label, input.state ?? TreeItemCollapsibleState.None)
 
     this.iconPath = input.icon
+    this.description = input.description
+    this.contextValue = input.contextValue
+    this.tooltip = input.label
     this.command = typeof input.command === 'string' ? {
       title: input.label,
       command: input.command,
