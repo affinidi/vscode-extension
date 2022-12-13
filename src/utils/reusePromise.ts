b6813fe7ee13e88beb8edf6b28b8238fd8eb534a
Vitaly Rudenko <vitaly.r@affinidi.com>
2022-12-13 16:35:10 +0200

    fix: improve reusePromise()

--- src/utils/reusePromise.ts
+++ src/utils/reusePromise.ts
@@ -1,23 +1,28 @@
+type Promisified<T> = Promise<Awaited<T>>
+
+function areArraysStrictEqual(a: unknown[], b: unknown[]) {
+  return (a.length === 0 && b.length === 0) || a.every((v, i) => v === b[i])
+}
+
 /**
  * Use this utility helps to avoid simultaneous duplicate requests.
  * It executes the method and stores the promise and execution arguments in memory.
  * Until the promise is resolved, calling the same method with the same arguments will reuse the existing promise.
  * This method is not always guaranteed to work because it compares the arguments using `===`.
  */
-export function reusePromise<T extends (...args: any[]) => any>(method: T) {
-  const executions: { args: Parameters<T>; promise: Promise<ReturnType<T>> }[] = []
+export function reusePromise<T extends (...args: Parameters<T>) => Promisified<ReturnType<T>>>(method: T) {
+  const executions: { args: Parameters<T>; promise: Promisified<ReturnType<T>> }[] = []
 
-  return async (...args: Parameters<T>): Promise<Awaited<ReturnType<T>>> => {
-    const existingExecution = executions.find(
-      (execution) =>
-        (execution.args.length === 0 && args.length === 0) ||
-        execution.args.every((arg, i) => arg === args[i]),
+  return (...args: Parameters<T>): Promise<Awaited<ReturnType<T>>> => {
+    const existingExecution = executions.find((execution) =>
+      areArraysStrictEqual(execution.args, args),
     )
+
     if (existingExecution) {
-      return await existingExecution.promise
+      return existingExecution.promise
     }
 
-    const promise = Promise.resolve(method(...args)).finally(() => {
+    const promise = method(...args).finally(() => {
       const index = executions.findIndex((execution) => execution.promise === promise)
       if (index !== -1) {
         executions.splice(index, 1)
@@ -25,6 +30,6 @@ export function reusePromise<T extends (...args: any[]) => any>(method: T) {
     })
 
     executions.push({ args, promise })
-    return await promise
+    return promise
   }
 }
