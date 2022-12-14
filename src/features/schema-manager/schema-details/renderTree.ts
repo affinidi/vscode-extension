23b85dc6b4b00c63824b1cc95616b0b68f40e9ef
OleksiiO-affinidi <87640022+OleksiiO-affinidi@users.noreply.github.com>
2022-12-14 10:49:44 +0200

    feat: add attributes for schema details webview (#68)
    * feat: add attributes for schema details webview
    
    * feat: review fixes

---
+++ src/features/schema-manager/schema-details/renderTree.ts
@@ -0,0 +1,47 @@
+import { SchemaField, SchemaFieldType } from '@affinidi/affinidi-vc-schemas'
+
+import { schemaMessage } from '../../../messages/messages'
+
+const ATTRIBUTE_TYPE_OPTIONS = [
+  { type: 'did', label: 'DID' },
+  { type: 'Text', label: 'Text' },
+  { type: 'URL', label: 'URL' },
+  { type: 'Date', label: 'Date' },
+  { type: 'DateTime', label: 'DateTime' },
+  { type: 'Number', label: 'Number' },
+  { type: 'Boolean', label: 'Boolean' },
+  { type: 'object', label: 'Object (nest attributes)' },
+]
+
+export const renderTree = (subFields?: SchemaField[], nestedLevel = 0): string | undefined =>
+  subFields
+    ?.map((field, index) => {
+      const selectedOption = ATTRIBUTE_TYPE_OPTIONS.find((option) => option.type === field?.type)
+      const hasSibling = !!subFields[index + 1]
+
+      return `
+        <div
+          class="attribute-box flex-1
+            ${nestedLevel === 0 ? 'parent-attribute' : ''}
+            ${nestedLevel > 0 ? 'attribute' : ''}
+            ${hasSibling ? 'has-sibling' : ''}
+          "
+        >
+          <div class="flex flex-1 attribute-details">
+            <div class="flex flex-1">${field.name}</div>
+            <div class="flex">
+              ${
+                selectedOption?.type !== SchemaFieldType.Object
+                  ? `<div class="attribute-type">${selectedOption?.label}</div>`
+                  : ''
+              }
+              <div class="attribute-required">
+                ${field.required ? schemaMessage.yes : schemaMessage.no}
+              </div>
+            </div>
+          </div>
+          ${field.nested?.length ? renderTree(field.nested, nestedLevel + 1) : ''}
+        </div>
+      `
+    })
+    .join('')
--- src/features/schema-manager/schema-details/showSchemaDetails.ts
+++ src/features/schema-manager/schema-details/showSchemaDetails.ts
@@ -1,10 +1,14 @@
+import { parseSchema, SchemaField } from '@affinidi/affinidi-vc-schemas'
 import { SchemaDto } from '@affinidi/client-schema-manager'
 import { Uri, ViewColumn, Webview, WebviewPanel, window } from 'vscode'
+import fetch from 'node-fetch'
+
 import { ext } from '../../../extensionVariables'
-import { labels } from '../../../messages/messages'
+import { labels, schemaMessage } from '../../../messages/messages'
 import { getWebviewUri } from '../../../utils/getWebviewUri'
 import { csvCreationService } from '../../issuance/csvCreationService'
 import { schemaManagerHelpers } from '../schemaManagerHelpers'
+import { renderTree } from './renderTree'
 
 let panel: WebviewPanel | undefined
 
@@ -14,10 +18,12 @@ function renderSchemaDetails({
   webview,
   extensionUri,
   schema,
+  schemaFields,
 }: {
   webview: Webview
   extensionUri: Uri
   schema: SchemaDto
+  schemaFields: SchemaField[]
 }) {
   const styleUri = getWebviewUri(webview, extensionUri, ['media', 'style.css'])
   const toolkitUri = getWebviewUri(webview, extensionUri, ['media', 'vendor', 'toolkit.js'])
@@ -56,7 +62,9 @@ function renderSchemaDetails({
             </div>
 
             <div class="box issuance-button-box">
-              <vscode-button id="${issuanceButtonId}">${labels.initiateIssuanceCsvFlow}</vscode-button>
+              <vscode-button id="${issuanceButtonId}">
+                ${labels.initiateIssuanceCsvFlow}
+              </vscode-button>
             </div>
           </div>
 
@@ -72,7 +80,30 @@ function renderSchemaDetails({
           <div class="box">
             <div class="title">JSON-LD CONTEXT</div>
             <div class="description">
-              <vscode-link href="${schema.jsonLdContextUrl}">${schema.jsonLdContextUrl}</vscode-link>
+              <vscode-link href="${schema.jsonLdContextUrl}">
+                ${schema.jsonLdContextUrl}
+              </vscode-link>
+            </div>
+          </div>
+
+          <vscode-divider></vscode-divider>
+
+          <div class="attribute-box">
+            <div class="flex flex-1">
+              <div class="attribute-box parent-attribute-header flex-1">
+                <div class="flex flex-1 attribute-details">
+                  <div class="flex flex-1 text-bold">${schemaMessage.attributeName}</div>
+
+                  <div class="flex">
+                    <div class="attribute-type text-bold">${schemaMessage.type}</div>
+                    <div class="attribute-required text-bold">${schemaMessage.required}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="attribute-box flex-1">
+              ${renderTree(schemaFields)}
             </div>
           </div>
         </section>
@@ -90,7 +121,13 @@ function renderSchemaDetails({
   `
 }
 
-export function showSchemaDetails({ projectId, schema }: { schema: SchemaDto; projectId: string }) {
+export async function showSchemaDetails({
+  projectId,
+  schema,
+}: {
+  schema: SchemaDto
+  projectId: string
+}) {
   if (!panel) {
     panel = window.createWebviewPanel('schemaDetailsView', '', ViewColumn.One, {
       enableScripts: true,
@@ -113,10 +150,15 @@ export function showSchemaDetails({ projectId, schema }: { schema: SchemaDto; pr
     )
   }
 
-  panel.title = `${labels.schema}: ${schemaManagerHelpers.getSchemaName(schema)}'`
+  panel.title = `${labels.schema}: ${schemaManagerHelpers.getSchemaName(schema)}`
+
+  const jsonSchema = await (await fetch(schema.jsonSchemaUrl)).json()
+  const { fields } = parseSchema(jsonSchema)
+
   panel.webview.html = renderSchemaDetails({
     webview: panel.webview,
     extensionUri: ext.context.extensionUri,
     schema,
+    schemaFields: fields,
   })
 }
