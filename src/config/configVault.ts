e93cd8ea21f7a7a575b129b32e1cbf68a9bb1582
Ezgi <88712585+ezgi-affinidi@users.noreply.github.com>
2023-01-03 15:18:12 +0100

    feat: checked the type of config files (#76)

--- src/config/configVault.ts
+++ src/config/configVault.ts
@@ -33,6 +33,10 @@ class ConfigVault {
     this.store.delete(key)
   }
 
+  getObject() {
+    return this.store.store
+  }
+
   async requireActiveProjectId(): Promise<string> {
     if (!this.getCurrentUserId()) {
       throw new NoCurrentUser()
@@ -94,6 +98,10 @@ class ConfigVault {
 
   setCurrentUserId(value: string): void {
     this.store.set('currentUserId', value)
+  }
+
+  getVersion(): number | undefined {
+    return this.store.get('version')
   }
 
   onCurrentUserIdChange(callback: OnDidChangeCallback<string | undefined>) {
