7b6e8a85f08d12079877c035f11f95420465e599
ezgi.a <ezgi.a@affinidi.com>
2023-01-10 13:59:34 +0100

    feat: formatting and removing unused messages

--- src/generators/messages.ts
+++ src/generators/messages.ts
@@ -1,12 +1,12 @@
 export const generatorMessage = {
-    failedToGenerateApp: 'Failed to generate app',
-    noDirectorySelected: "Installation folder wasn't selected",
-    appNameNotSelected: "App name wasn't specified",
-    checkCliInstall: 'Checking CLI installation...',
-    enterAppName: 'Enter an app name',
-    directoryNameDuplication: 'Directory with this name already exist',
-  }
+  failedToGenerateApp: 'Failed to generate app',
+  noDirectorySelected: "Installation folder wasn't selected",
+  appNameNotSelected: "App name wasn't specified",
+  checkCliInstall: 'Checking CLI installation...',
+  enterAppName: 'Enter an app name',
+  directoryNameDuplication: 'Directory with this name already exist',
+}
 
-  export const labels = {
-    select: 'Select',
-  }
+export const labels = {
+  select: 'Select',
+}
