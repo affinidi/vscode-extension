c68cc4a3c9a88b587633c203a6e0b5569ef40080
ezgi.a <ezgi.a@affinidi.com>
2023-01-17 12:44:36 +0100

    fix: changed config file validation

--- src/config/validateVaultFiles.ts
+++ src/config/validateVaultFiles.ts
@@ -6,35 +6,33 @@ import { configMessage } from './messages'
 
 const credentials = z.object({
   version: z.number(),
-  activeProjectSummary: z.optional(
-    z.object({
+  activeProjectSummary: z
+    .object({
       apiKey: z.object({ apiKeyHash: z.string(), apiKeyName: z.string() }),
       project: z.object({ createdAt: z.string(), name: z.string(), projectId: z.string() }),
       wallet: z.object({
         did: z.string(),
         didUrl: z.string(),
       }),
-    }),
-  ),
-  session: z.object({
-    sessionId: z.string(),
-    consoleAuthToken: z.string(),
-    account: z.object({
-      label: z.string(),
-      userId: z.string(),
-    }),
-    scopes: z.string().array(),
-  }),
+    })
+    .optional(),
+  session: z
+    .object({
+      sessionId: z.string(),
+      consoleAuthToken: z.string(),
+      account: z.object({
+        label: z.string(),
+        userId: z.string(),
+      }),
+      scopes: z.string().array(),
+    })
+    .optional(),
 })
 
 const config = z.object({
   version: z.number(),
-  currentUserId: z.string(),
-  configs: z.record(z.optional(z.object({ activeProjectId: z.string() }))),
-})
-
-const version = z.object({
-  version: z.number(),
+  currentUserId: z.string().optional(),
+  configs: z.record(z.optional(z.object({ activeProjectId: z.string() }))).optional(),
 })
 
 function isValidObject(objectType: any, object: any) {
@@ -42,15 +40,6 @@ function isValidObject(objectType: any, object: any) {
 }
 
 export function validateVaultFiles() {
-  // checking if extension installed for the first time
-  const checkOnlyVersionKeyInConfig =
-    isValidObject(version, configVault.getObject()) &&
-    Object.keys(configVault.getObject()).length === 1
-
-  if (checkOnlyVersionKeyInConfig) {
-    return
-  }
-
   const isConfigValid = isValidObject(config, configVault.getObject())
   const isCredentialsValid = isValidObject(credentials, credentialsVault.getObject())
 
